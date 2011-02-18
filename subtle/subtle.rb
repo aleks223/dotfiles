64955a0c2bfdf237c47572b86fb21f002c06e05e
aleks223 <aleks.christe@gmail.com>
2011-02-18 14:47:14 +0100

    modified:   subtle/subtle.rb

--- subtle/subtle.rb
+++ subtle/subtle.rb
@@ -1,11 +1,9 @@
 # Author::  s__C
-# Version:: 11.1.2011
-# License:: GNU GPL
 
-# == Options
 #
-# Following options change behaviour and sizes of the window manager:
+# == General options
 #
+
 # Border size in pixel of the windows
 set :border, 2
 
@@ -13,7 +11,7 @@ set :border, 2
 set :step, 2
 
 # Window screen border snapping
-set :snap, 0
+set :snap, 3
 
 # Default starting gravity for windows (0 = gravity of last client)
 set :gravity, :center
@@ -32,7 +30,7 @@ set :font, "-misc-fixed-medium-r-*-*-12-*-*-*-*-*-iso10646-*"
 #set :font, "xft:Pragmata-9"
 
 # Space around windows
-set :gap, 0
+set :gap, 2
 
 # Separator between sublets
 set :separator, ""
@@ -43,57 +41,20 @@ set :outline, 0
 #
 # == Screen
 #
-# Generally subtle comes with two panels per screen, one on the top and one at
-# the bottom. Each panel can be configured with different panel items and sublets
-# screen wise. Per default only the top panel on the first screen is used, it's
-# up to the user to enable the bottom panel or disable either one or both.
-#
-# Empty panels are hidden.
-#
-# Following items are available:
-#
-# [*:views*]     List of views with buttons
-# [*:title*]     Title of the current active window
-# [*:tray*]      Systray icons (Can be used once)
-# [*:sublets*]   Catch-all for installed sublets
-# [*:sublet*]    Name of a sublet for direct placement
-# [*:spacer*]    Variable spacer (free width / count of spacers)
-# [*:center*]    Enclose items with :center to center them on the panel
-# [*:separator*] Insert separator
-#
-# === Link
-#
-# http://subforge.org/wiki/subtle/Panel
-#
 
 screen 1 do
   # Add stipple to panels
   stipple false
 
   # Content of the top panel
-  top     [ :views, :title, :spacer, :mpd, :freq, :battery, :volume, :wifi, :clock, :tray]
+  top     [ :views, :title, :spacer, :mpd, :freq, :battery, :volume, :wifi, :gmail, :clock, :tray ]
 
   # Content of the bottom panel
-  # bottom  []
+  # bottom  [ :mail ]
 end
 
 #
 # == Colors
-#
-# Colors directly define the look of subtle, valid values are:
-#
-# [*hexadecimal*] #0000ff
-# [*decimal*]     (0, 0, 255)
-# [*names*]       blue
-#
-# Whenever there is no valid value for a color set - subtle will use a default
-# one. There is only one exception to this: If no background color is given no
-# color will be set. This will ensure a custom background pixmap won't be
-# overwritten.
-#
-# === Link
-#
-# http://subforge.org/wiki/subtle/Themes
 #
 
 # panel and separator
@@ -112,7 +73,7 @@ color :views_bg,      "#eeeeee"
 color :views_border,  "#eeeeee"
 
 # sublets
-color :fg_sublets,    "#555555"
+color :sublets_fg,    "#555555"
 color :sublets_bg,    "#eeeeee"
 color :sublets_border,"#eeeeee"
  
@@ -127,7 +88,7 @@ color :occupied_bg,      "#555555"
 color :occupied_border,  "#555555" 
 
 # urgent window titles and views
-color :urgent_fg,        "#eeeeee" 
+color :urgent_fg,        "#eeeeee"
 color :urgent_bg,        "#FFC400" 
 color :urgent_border,    "#FFC400" 
  
@@ -135,70 +96,50 @@ color :urgent_border,    "#FFC400"
 color :client_active,   "#0066ff" 
 color :client_inactive, "#555555"
 
-# Background color of root background
-#color :background,    "#3d3d3d"
+# panel
+color :panel, "#0066ff"
 
 #
 # == Gravities
 #
-# Gravities are predefined sizes a window can be set to. There are several ways
-# to set a certain gravity, most convenient is to define a gravity via a tag or
-# change them during runtime via grab. Subtler and subtlext can also modify
-# gravities.
-#
-# A gravity consists of four values which are a percentage value of the screen
-# size. The first two values are x and y starting at the center of the screen
-# and he last two values are the width and height.
-#
-# === Example
-#
-# Following defines a gravity for a window with 100% width and height:
-#
-#   gravity :example, [ 0, 0, 100, 100 ]
-#
-# === Link
-#
-# http://subforge.org/wiki/subtle/Gravity
-#
 
   # Gravities
-thirtythree = 1.0/3*100
 gravity :top_left,      [0, 0, 50, 50]
-gravity :top_left33,    [0, 0, thirtythree, thirtythree]
-gravity :top_left50,    [0, 0, thirtythree, 50]
+gravity :top_left33,    [0, 0, 34, 34]
+gravity :top_left50,    [0, 0, 34, 50]
 gravity :top,           [50, 0, 100, 50]
 gravity :partial_top,   [0,0, 100, 60]
-gravity :top33,         [50, 0, thirtythree, thirtythree]
-gravity :top50,         [50, 0, thirtythree, 50]
+gravity :top33,         [50, 0, 34, 34]
+gravity :top50,         [50, 0, 34, 50]
 gravity :top_right,     [100, 0, 50, 50]
-gravity :top_right33,   [100, 0, thirtythree, thirtythree]
-gravity :top_right50,   [100, 0, thirtythree, 50]
+gravity :top_right33,   [100, 0, 34, 34]
+gravity :top_right50,   [100, 0, 34, 50]
 gravity :left,          [0, 0, 50, 100]
 gravity :left66,        [0, 50, 25, 100]
-gravity :left50,        [0, 50, 50, thirtythree]
-gravity :left33,        [0, 50, thirtythree, thirtythree]
-gravity :leftw,         [0, 50, 2*thirtythree, 100]
+gravity :left50,        [0, 50, 50, 34]
+gravity :left33,        [0, 50, 34, 34]
+gravity :leftw,         [0, 50, 2*34, 100]
 gravity :lefth,         [0, 50, 75, 100]
 gravity :center,        [0, 0, 100, 100]
-gravity :center66,      [0, 50, 100, thirtythree]
-gravity :center33,      [50, 50, 50, thirtythree]
+gravity :center66,      [0, 50, 100, 34]
+gravity :center33,      [50, 50, 50, 34]
 gravity :center75,      [50, 50, 50, 100]
 gravity :right,         [100, 0, 50, 100]
 gravity :right66,       [100, 50, 25, 100]
-gravity :right50,       [100, 50, 50, thirtythree]
-gravity :right33,       [100, 50, thirtythree, thirtythree]
+gravity :right50,       [100, 50, 50, 34]
+gravity :right33,       [100, 50, 34, 34]
 gravity :rightw,        [100, 0, 60, 100]
-gravity :righth,        [100, 50, 2*thirtythree, 100]
+gravity :righth,        [100, 50, 2*34, 100]
 gravity :bottom_left,   [0, 100, 50, 50]
-gravity :bottom_left50, [0, 100, thirtythree, 50]
-gravity :bottom_left33, [0, 100, thirtythree, thirtythree]
+gravity :bottom_left50, [0, 100, 34, 50]
+gravity :bottom_left33, [0, 100, 34, 34]
 gravity :bottom,        [0, 100, 100, 50]
 gravity :partial_bottom,[0, 100, 100, 40]
-gravity :bottom66,      [50, 100, thirtythree, 50]
-gravity :bottom33,      [50, 100, thirtythree, thirtythree]
+gravity :bottom66,      [50, 100, 34, 50]
+gravity :bottom33,      [50, 100, 34, 34]
 gravity :bottom_right,  [100, 100, 50, 50]
-gravity :bottom_right50, [100, 100, thirtythree, 50]
-gravity :bottom_right33, [100, 100, thirtythree, thirtythree]
+gravity :bottom_right50, [100, 100, 34, 50]
+gravity :bottom_right33, [100, 100, 34, 34]
   # Gimp
 gravity :gimp_image,     [  50,  50,  80, 100 ]
 gravity :gimp_toolbox,   [   0,   0,  10, 100 ]
@@ -208,79 +149,6 @@ gravity :scratch, [ 46, 85, 46, 20 ]
 #
 # == Grabs
 #
-# Grabs are keyboard and mouse actions within subtle, every grab can be
-# assigned either to a key and/or to a mouse button combination. A grab
-# consists of a chain and an action.
-#
-# === Finding keys
-#
-# The best resource for getting the correct key names is
-# */usr/include/X11/keysymdef.h*, but to make life easier here are some hints
-# about it:
-#
-# * Numbers and letters keep their names, so *a* is *a* and *0* is *0*
-# * Keypad keys need *KP_* as prefix, so *KP_1* is *1* on the keypad
-# * Strip the *XK_* from the key names if looked up in
-#   /usr/include/X11/keysymdef.h
-# * Keys usually have meaningful english names
-# * Modifier keys have special meaning (Alt (A), Control (C), Meta (M),
-#   Shift (S), Super (W))
-#
-# === Chaining
-#
-# Chains are a combination of keys and modifiers to one key and can be used in
-# various ways to trigger an action. In subtle there are two ways to define
-# chains for grabs:
-#
-#   1. Default way*: Add modifiers to a key and use it for a grab
-#
-#      *Example*: grab "W-Return", "urxvt"
-#
-#   2. *Escape way*: Define an escape grab that needs to be pressed before
-#      *any* other grab can be used like in screen/tmux.
-#
-#      *Example*: grab "C-y", :SubtleEscape
-#                 grab "Return", "urxvt"
-#
-# ==== Mouse buttons
-#
-# [*B1*] = Button1 (Left mouse button)
-# [*B2*] = Button2 (Middle mouse button)
-# [*B3*] = Button3 (Right mouse button)
-# [*B4*] = Button4 (Mouse wheel up)
-# [*B5*] = Button5 (Mouse wheel down)
-#
-# ==== Modifiers
-#
-# [*A*] = Alt key
-# [*C*] = Control key
-# [*M*] = Meta key
-# [*S*] = Shift key
-# [*W*] = Super (Windows) key
-#
-# === Action
-#
-# An action is something that happens when a grab is activated, this can be one
-# of the following:
-#
-# [*symbol*] Run a subtle action
-# [*string*] Start a certain program
-# [*array*]  Cycle through gravities
-# [*lambda*] Run a Ruby proc
-#
-# === Example
-#
-# This will create a grab that starts a urxvt when Alt+Enter are pressed:
-#
-#   grab "A-Return", "urxvt"
-#
-# === Link
-#
-# http://subforge.org/wiki/subtle/Grabs
-#
-
-# Escape grab
-#  grab "C-y", :SubtleEscape
 
 # Launcher
 begin
@@ -289,7 +157,10 @@ begin
    # Set fonts
    Subtle::Contrib::Launcher.fonts = [
      "xft:Pragmata:pixelsize=50:antialias=true",
-     "xft:Pragmata:pixelsize=25:antialias=true" 
+     "xft:Pragmata:pixelsize=25:antialias=true"
+   ]
+   Subtle::Contrib::Launcher.paths = [
+     "/usr/bin","/home/alex/Matlab/bin"
    ]
  rescue LoadError => error
    puts error
@@ -396,8 +267,9 @@ grab "W-e", "gedit"
 grab "W-c", "iron"
 grab "W-g", "gvim"
 grab "W-h", "hotot"
-grab "W-m", "mpd"
-grab "W-n", "urxvtc -name ncmpcpp -e ncmpcpp"
+grab "W-m", "urxvtc -name mutt -e mutt"
+grab "W-C-m", "urxvtc -e matlab -nosplash -desktop"
+grab "W-n", "mpd && urxvtc -name ncmpcpp -e ncmpcpp || urxvtc -name ncmpcpp -e ncmpcpp"
 grab "W-t", "urxvtc -name tmux -e tmux"
 grab "W-a", "urxvtc -name tmux -e tmux a || tmux"
 grab "W-f", "filezilla"
@@ -433,105 +305,6 @@ end
 #
 # == Tags
 #
-# Tags are generally used in subtle for placement of windows. This placement is
-# strict, that means that - aside from other tiling window managers - windows
-# must have a matching tag to be on a certain view. This also includes that
-# windows that are started on a certain view will not automatically be placed
-# there.
-#
-# There are to ways to define a tag:
-#
-# [*string*]  With a WM_CLASS/WM_NAME
-# [*hash*]    With a hash of properties
-#
-# === Default
-#
-# Whenever a window has no tag it will get the default tag and be placed on the
-# default view. The default view can either be set by the user with adding the
-# default tag to a view by choice or otherwise the first defined view will be
-# chosen automatically.
-#
-# === Properties
-#
-# Additionally tags can do a lot more then just control the placement - they
-# also have properties than can define and control some aspects of a window
-# like the default gravity or the default screen per view.
-#
-# [*float*]   This property either sets the tagged client floating or prevents
-#              it from being floating depending on the value.
-#
-#              Example: float true
-#
-# [*full*]    This property either sets the tagged client to fullscreen or
-#              prevents it from being set to fullscreen depending on the value.
-#
-#              Example: full true
-#
-# [*gravity*] This property sets a certain to gravity to the tagged client,
-#              but only on views that have this tag too.
-#
-#              Example: gravity :center
-#
-# [*match*]   This property adds matching patterns to a tag, a tag can have
-#              more than one. Matching works either via plaintext, regex
-#              (see man regex(7)) or window id. Per default tags will only
-#              match the WM_NAME and the WM_CLASS portion of a client, this
-#              can be changed with following possible values:
-#
-#              [*:name*]      Match the WM_NAME
-#              [*:instance*]  Match the first (instance) part from WM_CLASS
-#              [*:class*]     Match the second (class) part from WM_CLASS
-#              [*:role*]      Match the window role
-#
-#              Example: match :instance => "urxvt"
-#                       match [:role, :class] => "test"
-#                       match "[xa]+term"
-#
-# [*exclude*] This property works exactly the same way as *match*, but it
-#              excludes clients that match from this tag. That can be helpful
-#              with catch-all tags e.g. for console apps.
-#
-#              Example: exclude :instance => "irssi"
-#
-# [*resize*]  This property either enables or disables honoring of client
-#              resize hints and is independent of the global option.
-#
-#              Example: resize true
-#
-# [*size*]    This property sets a certain to size as well as floating to the
-#              tagged client, but only on views that have this tag too. It
-#              expects an array with x, y, width and height values.
-#
-#              Example: size [100, 100, 50, 50]
-#
-# [*stick*]   This property either sets the tagged client to stick or prevents
-#              it from being set to stick depending on the value. Stick clients
-#              are visible on every view.
-#
-#              Example: stick true
-#
-# [*type*]    This property sets the [[Tagging|tagged]] client to be treated
-#              as a specific window type though as the window sets the type
-#              itself. Following types are possible:
-#
-#              [*:desktop*]  Treat as desktop window (_NET_WM_WINDOW_TYPE_DESKTOP)
-#              [*:dock*]     Treat as dock window (_NET_WM_WINDOW_TYPE_DOCK)
-#              [*:toolbar*]  Treat as toolbar windows (_NET_WM_WINDOW_TYPE_TOOLBAR)
-#              [*:splash*]   Treat as splash window (_NET_WM_WINDOW_TYPE_SPLASH)
-#              [*:dialog*]   Treat as dialog window (_NET_WM_WINDOW_TYPE_DIALOG)
-#
-#              Example: type :desktop
-#
-# [*urgent*]  This property either sets the tagged client to be urgent or
-#              prevents it from being urgent depending on the value. Urgent
-#              clients will get keyboard and mouse focus automatically.
-#
-#              Example: urgent true
-#
-# === Link
-#
-# http://subforge.org/wiki/subtle/Tagging
-#
 
 # Simple tags
 tag "www", "uzbl|iron|opera|firefox|chromium|dwb"
@@ -541,12 +314,26 @@ tag "vb", "virtualbox"
 # Placement
 tag "term" do
   match "xterm|[u]?rxvt"
-  exclude "vim|scratch"
+  exclude "vim|scratch|mutt|tmux|ncmpcpp|screen"
 end
 
 tag "vim" do
   match "vim"
   gravity :right
+end
+
+tag "tmux" do
+  match "tmux|screen"
+  gravity :partial_top
+end
+
+tag "mpd" do
+  match "ncmpcpp"
+  gravity :partial_bottom
+end
+
+tag "mail" do
+  match "mutt"
 end
 
 tag "scratchpad" do
@@ -567,8 +354,6 @@ end
 
 tag "matlab" do
   match :class => ".*mat(lab|hworks).*"
-  resize true  
-  float true
 end
 
 tag "flash" do
@@ -578,11 +363,12 @@ end
 
 tag "editor" do
   match  "gvim|gedit|texmaker|eclipse|geany"
-  resize true
+  resize false
 end
 
 tag "view" do
   match "zathura|evince"
+  float true
   resize true
 end
 
@@ -638,18 +424,9 @@ end
 #
 # == Views
 #
-# Views are the virtual desktops in subtle, they show all windows that share a
-# tag with them. Windows that have no tag will be visible on the default view
-# which is the view with the default tag or the first defined view when this
-# tag isn't set.
-#
-# === Link
-#
-# http://subforge.org/wiki/subtle/Tagging
-#
 
 view "term" do
-  match "term|vim"
+  match "term|mpd|tmux"
   dynamic false
   icon "#{ENV["HOME"]}/.local/share/subtle/icons/terminal.xbm"
   icon_only true
@@ -662,12 +439,12 @@ view "www" do
   icon_only true
 end
 
-#view "media" do
-#  match "mpd"
-#  dynamic "false"
-#  icon "#{ENV["HOME"]}/.local/share/subtle/icons/note1.xbm"
-#  icon_only true
-#end
+view "mail" do
+  match "mail"
+  dynamic false
+  icon "#{ENV["HOME"]}/.local/share/subtle/icons/mail.xbm"
+  icon_only true
+end
 
 view "design" do
   match "vec|gimp_.*"
@@ -677,14 +454,14 @@ view "design" do
 end
 
 view "dev" do
-  match "editor|view"
+  match "editor|vim"
   dynamic false
   icon "#{ENV["HOME"]}/.local/share/subtle/icons/notepad.xbm"
   icon_only true
 end
 
 view "bullshit" do
-  match "default|hotot|fm|matlab"
+  match "default|hotot|fm"
   dynamic false
   icon "#{ENV["HOME"]}/.local/share/subtle/icons/quote.xbm"
   icon_only true
@@ -697,10 +474,19 @@ view "vbox" do
   icon_only true
 end
 
+view "prog" do
+  match "matlab"
+  dynamic true
+  icon "#{ENV["HOME"]}/.local/share/subtle/icons/terminal.xbm"
+  icon_only true
+end
+
+#
 # Sublets
+#
 
 sublet :clock do
-  foreground "#ff6565"
+  foreground "#ce5c00"
   format_string "%d.%m.%y %H:%M"
 end
 
@@ -709,13 +495,20 @@ sublet :volume do
 end
 
 sublet :wifi do
-  interval 120
   foreground "#ffc123"
 end
 
 sublet :freq do
   interval 1
-  foreground "#ce5c00"
+  foreground "#ff6565"
+end
+
+sublet :gmail do
+  user "USER@gmail.com"
+  password "PASS"
+  foreground "#555753"
+  urgent "#ffc400"
+  normal "#eeeeee"
 end
 
 sublet :battery do
@@ -731,42 +524,3 @@ sublet :fuzzytime do
   foreground "#ff6565"
   locale "fr"
 end
-
-# == Hooks
-#
-# And finally hooks are a way to bind Ruby scripts to a certain event.
-#
-# Following hooks exist so far:
-#
-# [*:client_create*]    Called whenever a window is created
-# [*:client_configure*] Called whenever a window is configured
-# [*:client_focus*]     Called whenever a window gets focus
-# [*:client_kill*]      Called whenever a window is killed
-#
-# [*:tag_create*]       Called whenever a tag is created
-# [*:tag_kill*]         Called whenever a tag is killed
-#
-# [*:view_create*]      Called whenever a view is created
-# [*:view_configure*]   Called whenever a view is configured
-# [*:view_jump*]        Called whenever the view is switched
-# [*:view_kill*]        Called whenever a view is killed
-#
-# [*:tile*]             Called on whenever tiling would be needed
-# [*:reload*]           Called on reload
-# [*:start*]            Called on start
-# [*:exit*]             Called on exit
-#
-# === Example
-#
-# This hook will print the name of the window that gets the focus:
-#
-#   on :client_focus do |c|
-#    puts c.name
-#   end
-#
-# === Link
-#
-# http://subforge.org/wiki/subtle/Hooks
-#
-
-# vim:ts=2:bs=2:sw=2:et:fdm=marker
